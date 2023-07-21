2c69885c1ee79ecf4678bb8a4e05c8b4060bdd4d
CAKRA <kyuackerman30@gmail.com>
2023-07-21 13:00:48 +0700

    Update Middleware Throttle (Limit request by time) with Cache v1

--- Middleware/Limit request Cache/ThrottleRequests.php
+++ Middleware/Limit request Cache/ThrottleRequests.php
@@ -25,22 +25,28 @@ class ThrottleRequests
         if (!$apiKey) {
             return response()->json(['Message' => 'Invalid API key'], 401);
         }
-        $rateLimit = 10;
+        $rateLimit = 100;
         $timer = 60;
         $requests = Cache::get($key, 0);
         $lastRequestTime = Cache::get($key . ':timer');
-        if ($requests >= $rateLimit) {
-            if (Cache::has($key . ':timer') && (time() - $lastRequestTime) > $timer) {
-                Cache::put($key, 0, now()->addMinute());
-                $requests = 0;
-            } else {
-                return response()->json(['message' => 'Rate limit exceeded'], 429);
-            }
+        if ($lastRequestTime == null) {
+            Cache::put($key, 0, now()->addMinute());
+            $requests = 0;
+            Cache::put($key . ':timer', time(), now()->addMinute());
         } else {
-            Cache::increment($key);
-        }
+            if ($requests >= $rateLimit) {
+                if (Cache::has($key . ':timer') && (time() - $lastRequestTime) > $timer) {
+                    Cache::put($key, 0, now()->addMinute());
+                    $requests = 0;
+                } else {
+                    return response()->json(['message' => 'Rate limit exceeded'], 429);
+                }
+            } else {
+                Cache::increment($key);
+            }
 
-        Cache::put($key . ':timer', time(), now()->addMinute());
+            Cache::put($key . ':timer', time(), now()->addMinute());
+        }
         return $next($request);
     }
 }
